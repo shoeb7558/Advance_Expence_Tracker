c15fd2b1fc1766fc353787a7a8cbc46965f116fa
shoeb7558 <shoeb9530@gmail.com>
2024-01-16 16:46:19 +0530

    this is the updated verion 5.0

--- src/Expences/Expences.jsx
+++ src/Expences/Expences.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ExpencesModule.css'
 
 const ExpenseTracker = () => {
@@ -7,9 +7,10 @@ const ExpenseTracker = () => {
   const [amount, setAmount] = useState('');
   const [description, setDescription] = useState('');
   const [category, setCategory] = useState('');
+  // const [data, setdata] = useState([])
 
   // Function to handle form submission
-  const addExpense = (e) => {
+  const addExpense = async (e) => {
     e.preventDefault();
 
     // Check if all input fields are filled
@@ -28,12 +29,47 @@ const ExpenseTracker = () => {
 
     // Update the expenses state with the new expense
     setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+    const response = await fetch('https://advanceexpencetracker-default-rtdb.firebaseio.com/Expences.json', {
+      method: 'POST',
+      body: JSON.stringify(newExpense),
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+    const responseData = await response.json();
+    console.log(responseData);
+   
 
     // Clear input fields
     setAmount('');
     setDescription('');
     setCategory('');
   };
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      
+      try {
+        const response = await fetch('https://advanceexpencetracker-default-rtdb.firebaseio.com/Expences.json');
+        if (!response.ok) {
+          throw new Error('Something went wrong!');
+        }
+        const result = await response.json();
+        // Convert the Firebase data object into an array of movies
+        const Expences = result ? Object.keys(result).map((key) => ({ id: key, ...result[key] })) : [];
+        setExpenses(Expences);
+        // console.log(Expences);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        
+      } finally {
+       console.log('done')
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className='expencediv1'>
@@ -90,8 +126,8 @@ const ExpenseTracker = () => {
 
       <ul>
         {/* Map through expenses and display them in list items */}
-        {expenses.map((expense) => (
-          <li key={expense.id} className='expenceli'>
+        {expenses.map((expense, index) => (
+          <li key={index} className='expenceli'>
              <span><strong>AMOUNT : </strong>${expense.amount.toFixed(2)}</span>
              <span><strong>DESCRIPTION : </strong>{expense.description}</span>
              <span><strong>CATEGORY : </strong>{expense.category}</span>
