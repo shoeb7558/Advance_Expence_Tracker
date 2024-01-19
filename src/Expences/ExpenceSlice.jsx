b258d85848402271c5bebb1c2b33afcb30097dc7
shoeb7558 <shoeb9530@gmail.com>
2024-01-19 17:06:57 +0530

    dark theme

--- src/Expences/ExpenceSlice.jsx
+++ src/Expences/ExpenceSlice.jsx
@@ -6,6 +6,8 @@ const expensesSlice = createSlice({
   name: 'expenses',
   initialState: {
     expensesList: [],
+    showPremiumButton: false,
+    darkMode: false,
   },
   reducers: {
     setExpenses(state, action) {
@@ -14,8 +16,14 @@ const expensesSlice = createSlice({
     addExpense(state, action) {
       state.expensesList.push(action.payload);
     },
+    setPremiumButtonVisibility: (state, action) => {
+      state.showPremiumButton = action.payload;
+    },
+    toggleTheme: (state) => {
+      state.darkMode = !state.darkMode;
+    },
   },
 });
-
+export const { toggleTheme } = expensesSlice.actions;
 export const { setExpenses, addExpense } = expensesSlice.actions;
 export default expensesSlice.reducer;
